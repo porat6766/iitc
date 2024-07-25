41818524cac32255e48c6e2c6d5cc21802b62c8c
porat6766 <porat850@gmail.com>
2024-07-25 09:59:55 +0300

    tets

--- main.js
+++ main.js
@@ -1,13 +1,129 @@
-//quetion 1
+//question 1
 let age = 25;
 console.log (age);
 
-// quetion 2
-
+// question 2
 let Name = "porat";
 console.log("firstname");
 
-//quetion 3
+//question 3
 const pi = 3.14159;
 console.log (pi);
+
+//question 4
+let x = 1, y = 2, z =3;
+console.log (x);
+console.log (y);
+console.log (z);
+
+//quetion 5
+let isStudent = true;
+console.log (isStudent);
+
+//question 6
+let favoriteColor;
+console.log (favoriteColor) 
+ 
+//question 7
+let temperature = 72.5;
+console.log(temperature);
+
+//question 8
+const days_in_week = 7;
+console.log (days_in_week);
+
+//question 9
+let userName = "coder123";
+console.log (userName);
+
+//question 10
+let score = 0;
+console.log(score);
+
+//question 11
+let isLoggedIn = false;
+console.log(isLoggedIn);
+
+//question 12
+const campany_name = "tech solution inc.";
+console.log (campany_name);
+
+//question 13
+let currentYear =  2024;
+console.log(currentYear);
+
+//question 14
+let price = 19.99;
+console.log(price);
+
+//question 15
+let quantity = 100;
+console.log(quantity)
+
+//question 16
+let counter = 0;
+console.log(0);
+console.log(counter + 1);
+
+//question 17
+let message = "hello";
+console.log(message);
+message = "hello, world";
+console.log(message);
+
+//question 18
+let isAvailable = true;
+console.log(isAvailable);
+
+isAvailable = false;
+console.log(isAvailable);
+
+//question 19
+let totalcost = 50;
+console.log(totalcost);
+totalcost = totalcost + 10;
+console.log(totalcost);
+
+//question 20
+let username = "user1";
+console.log(username);
+
+username = "admin";
+console.log(username);
+
+//question 21
+let Temperature = 20;
+console.log(Temperature);
+Temperature = Temperature * 1.8 + 32;
+console.log(Temperature);
+
+//question 22
+let fruits = ("apple, banana");
+console.log(fruits);
+fruits = ("apple, banana, orange");
+console.log(fruits);
+
+//question 23
+let person = "name, AGE"
+console.log(person);
+person = "name, age, city";
+console.log(person);
+
+//question 24
+let count = 5;
+console.log(count);
+count -= 1
+console.log(count);
+
+//question 25
+let greeting = "good";
+console.log(greeting);
+greeting = "good morning";
+console.log(greeting);
+
+
+
+
+
+
 
