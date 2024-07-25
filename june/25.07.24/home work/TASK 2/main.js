0bb6c4087c32aa200e313ba3bd2d331ab969e569
porat6766 <porat850@gmail.com>
2024-07-25 18:01:35 +0300

    home worl 3 last ones

---
+++ june/25.07.24/home work/TASK 2/main.js
@@ -0,0 +1,25 @@
+//1.number
+//2.3 types of quotation marks "/~/'
+//3.undefined its varieble not define,and null its failf in the solution(kind of error)
+//4.with the help of "typeof"
+//5.number
+//6.boolean
+//7.{}
+//8.number
+//9.absulute compare or specific the same(===)
+//10.parseInt()
+//11."53"
+//12.typeof
+//13.in using template literals
+//14.singgle and double quotes its the same,we use them to wrhrite classic string(sesens,Letters, numbers and signs.)and backtickts we using when we have muli-line
+//15.using quotes,i think all the types
+//16.object
+//17.const
+//18.its depend in some case,this become everiting to string,and some case everiting to number,or its present eror
+//19.boolean
+//20.
+//21.when you put len- you can in the nexts code line to change the virables,and in constant its fixed
+//22.chek with tupeof
+//23.everyting became to string-so 105
+//24.you need to set the value true or false
+//25.when you use premitive data typse you just move the valuble and  when you use in object  thus take you to a lot of data or to function that do complecated calculation.
