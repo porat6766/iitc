2d2c3c4b71cd49329d1d8b00937bf1326ea13e55
porat6766 <porat850@gmail.com>
2024-10-01 16:12:42 +0300

    home-work-array-methods

--- september/29.09.24/HomeWork/main.js
+++ september/29.09.24/HomeWork/main.js
@@ -327,8 +327,307 @@
 // console.log(justWithA);
 
 //44.
-const chrs = ["א", "ב", "א", "ג", "ב", "א"];
-const createObj = chrs.reduce((calc, char) => {
-  return calc[char] ? calc[char]++ : (calc[char] = 1), calc;
-}, {});
-console.log(createObj);
+// const chrs = ["א", "ב", "א", "ג", "ב", "א"];
+// const createObj = chrs.reduce((calc, char) => {
+//   return calc[char] ? calc[char]++ : (calc[char] = 1), calc;
+// }, {});
+// console.log(createObj);
+// const order = chrs.reduce((calc, char) => {
+//   return calc[char] ? calc[char]++ : (calc[char] = 1), calc;
+// }, {});
+// console.log(order);
+
+//45.
+// const strs = ["שלום", "עולם", "גאווה", "סקריפט"];
+// const check = strs.some((str) => {
+//   return str.includes("ז");
+// });
+// console.log(check);
+
+//46.
+// const numbers = [2, 4, 6, 8];
+// const checkAll = numbers.every((num) => {
+//   return num % 2 === 0;
+// });
+// console.log(checkAll);
+
+//47.
+// const accounts = [
+//   { id: 1, status: "לא פעיל" },
+//   { id: 2, status: "פעיל" },
+//   { id: 2, status: "פעיל" },
+//   { id: 2, status: "פעיל" },
+//   { id: 2, status: "פעיל" },
+// ];
+// const checkActive = accounts.find((account) => {
+//   return account.status === "פעיל";
+// });
+// console.log(checkActive);
+
+//48.
+// const numbers = [3, 7, 2, 9, -5];
+// const found = numbers.findIndex((num, index) => {
+//   return index % 2 === 0 ? num < 0 : num < 0;
+// });
+// console.log(found);
+
+//49.
+// const strts = ["JavaScript", "Python", "Ruby", "Java"];
+// const order = strts.sort((a, b) => {
+//   return a.length - b.length;
+// });
+// console.log(order);
+
+//50.
+// const arraysodinmantinal = [1, [2, [3]], [4, [5]]];
+// const clean = arraysodinmantinal.flat();
+// console.log(clean);
+
+//51.
+
+// const chars = ["ש", "ל", "ו", "ם"];
+// let string = "";
+// const strFun = chars.forEach((str) => {
+//   string += str;
+// });
+// console.log(string);
+
+//52.
+// const numbers = [1, 2, 3, 4, 5];
+// const AddToNum = numbers.map((num) => {
+//   return num + 10;
+// });
+// console.log(AddToNum);
+
+//53.
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const chek = numbers.filter((num) => num % 3 === 0);
+// console.log(chek);
+
+//54.
+// const strs = ["קצר", "בינוני", "הכי ארוך", "ארוך יותר"];
+// const longest = strs.reduce((calc, str) => {
+//   return calc.length < str.length ? str : calc;
+// }, "");
+// console.log(longest);
+
+//55.
+// const numbers = [1, 3, 5, 7, 9];
+// const check = numbers.some((num) => {
+//   return num % 2 === 0;
+// });
+// console.log(check);
+
+//56.
+// const strs = ["תפוח", "תפ", "תפוז"];
+// const check = strs.every((str) => {
+// return str[0] === "ת" && str[1] === "פ";
+// });
+// console.log(check);
+
+// const strs = ["תפוח", "תפ", "תפוז"];
+// const check = strs.every((str) => {
+//   return str.startsWith("תפ");
+// });
+// console.log(check);
+
+//57.
+// const toDoLIst = [
+//   { id: 1, completed: false },
+//   { id: 2, completed: true },
+// ];
+// const check = toDoLIst.find((toDo) => {
+//   return toDo.completed === true;
+// });
+// console.log(check);
+
+//58.
+// const fruits = ["תפוח", "בננה", "דובדבן"];
+// const check = fruits.findIndex((fruit) => fruit === "בננה");
+// console.log(check);
+
+//59.
+// const students = [
+//   { name: "יוחנן", age: 25 },
+//   { name: "יעל", age: 30 },
+//   { name: "בועז", age: 20 },
+// ];
+// const checks = students.sort((a, b) => a.name.localeCompare(b.name));
+// console.log(checks);
+// const create = students.map((student) => student.name);
+// console.log(create);
+// const orderCreat = create.sort();
+// console.log(orderCreat);
+
+//60.
+// const array4Diomen = [1, [2, 3], [4, [5, 6]]];
+// const order = array4Diomen.flat(2);
+// console.log(order);
+
+//61.
+// let str = "שלום";
+// [...str].forEach((char) => console.log(char));
+// str.split("").forEach((char) => console.log(char));
+
+//62.
+// const fruits = ["תפוח", "בננה", "דובדבן"];
+// const firstLetter = fruits.map((fruit) => fruit[0]);
+// console.log(firstLetter.sort());
+
+//63.
+// const letters = ["א", "אב", "אבג", "אבגד"];
+// const fustThreeLetters = letters.filter((str) => str.length >= 3);
+// console.log(fustThreeLetters);
+
+//64.
+// const fruits = ["תפוח", "בננה", "דובדבן"];
+// const sum = fruits.reduce((calc, str) => calc + str.length, 0);
+// console.log(sum);
+
+//65.
+// const greeting = ["שלום", "עולם", "גאווהסקריפט"];
+// const check = greeting.some((str) => str.length > 10);
+// console.log(check);
+
+//66.
+// const someNumbers = [10, 20, 30, 40, 50];
+// const check = someNumbers.every((num) => num > 5);
+// console.log(check);
+
+//67.
+// const words = ["ספר", "דלת", "חלוון"];
+// const find = words.find((str) => str.includes("וו"));
+// console.log(find);
+
+//68.
+// const numbers = [5, 10, 15, 20];
+// const found = numbers.findIndex((num) => num >= 10);
+// console.log(found);
+
+//69.
+// const letters = ["אאא", "בב", "ג", "א"];
+// const order = letters.sort();
+// console.log(order);
+
+//70.
+// const arrayDiomentional = [1, [2, [3, [4]]]];
+// const order = arrayDiomentional.flat(3);
+// console.log(order);
+
+//71.
+// const numbers = [1, 4, 9, 16];
+// const newA = [];
+// numbers.forEach((num) => newA.push(Math.sqrt(num)));
+// console.log(newA);
+
+//72.
+// const letters = ["א", "ב", "ג"];
+// const newLetters = letters.map((char) => char + char);
+// const newLetters = letters.map((char) => char.repeat(2));
+// const newLetters = letters.map((char) => [char, char]).flat(2);
+// console.log(newLetters);
+
+//73.
+// const numbers = [5, 10, 15, 20, 25];
+// const newArray = numbers.filter((num) => num <= 20 && num >= 10);
+// console.log(newArray);
+
+//74.
+// const lettChar = [{ א: 1 }, { ב: 2 }, { ג: 3 }];
+// const newObj = lettChar.reduce((calc, current) => {
+//   return { ...calc, ...current };
+// });
+// console.log(newObj);
+
+//75.
+// const charNum = [{ x: 1 }, { y: 2 }, { z: 3 }];
+// const check = charNum.some((obj) => obj.y);
+// console.log(check);
+
+//76.
+// const englishLetters = "אבגדהוזחטיכלמנסעפצקרשתףץךם";
+// const numbers = "0123456789";
+// const charNum = ["א1", "ב2", "ג3"];
+// const check = charNum.every((str) => {
+//   const charOn = str[0];
+//   const charse = str[1];
+//   console.log(charOn);
+
+//   return numbers.includes(charse) && englishLetters.includes(charOn);
+// });
+// console.log(check);
+
+//77.
+// const objects = [{ מחיר: 60 }, { מחיר: 40 }, { מחיר: 70 }];
+// const findPricw = objects.find((obj) => obj.מחיר < 50);
+// console.log(findPricw);
+
+//78.
+// const str = [1, "", true, 0, null, "שלום"];
+// const found = str.findIndex((some) => !some);
+// console.log(found);
+
+//79.
+// const numbers = [3.14, 2.71, 1.41, 1.73];
+
+// const newN = numbers.map((num) => Math.round(num));
+// console.log(newN.sort());
+
+// const order = numbers.sort((a, b) => Math.round(a) - Math.round(b));
+// console.log(order);
+
+//80.
+// const strs = ["א", ["ב", ["ג"]]];
+// const clesn = strs.flat(Infinity);
+// console.log(clesn);
+
+//81.
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const newArrNum = numbers.map((num) => num ** 2).filter((num) => num % 2 === 0);
+// console.log(newArrNum);
+
+//82.//////////////////////////////////////////
+// const stusents = [
+//   { שם: "אליס", גיל: 25 },
+//   { שם: "בוב", גיל: 30 },
+//   { שם: "צרלי", גיל: 25 },
+// ];
+
+// const shdbf = stusents.reduce((calc, obCurrent) => {
+//   !calc[obCurrent.גיל]
+//     ? (calc[obCurrent.גיל] = [obCurrent])
+//     : calc[obCurrent.גיל].push(obCurrent);
+//   return calc;
+// }, {});
+// console.log(shdbf);
+
+//83.
+// function debounce(fn, delay) {
+//   let timeoutId;
+//   return function (...args) {
+//     clearTimeout(timeoutId);
+//     timeoutId = setTimeout(() => {
+//       fn(...args);
+//     }, delay);
+//   };
+// }
+
+// function greeting() {
+//   console.log("hi");
+// }
+// function bye() {
+//   console.log("bi");
+// }
+// const arrays = [greeting, bye];
+// arrays.forEach((arr, index) => {
+//   if (index === 0) {
+//     const play = debounce(arr, 1000);
+//     play();
+//   }
+//   if (index === 1) {
+//     const play = debounce(arr, 2000);
+//     play();
+//   }
+// });
+
+//84.
