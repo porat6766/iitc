5089825e906f224e0a32a2b47121e9e37e189af6
porat6766 <porat850@gmail.com>
2024-10-09 09:38:06 +0300

    finish project Faqs

---
+++ Project FAQ accordion/faq-accordion-main/main.js
@@ -0,0 +1,21 @@
+const elPlus = document.querySelectorAll(".plus");
+const elMinus = document.querySelectorAll(".minus");
+const elPar = document.querySelectorAll(".paragaph");
+
+for (let i = 0; i < elPlus.length; i++) {
+  let elpl = elPlus[i];
+  elpl.addEventListener("click", () => {
+    toggleToParaAndPlusAndMinus(i);
+  });
+}
+function toggleToParaAndPlusAndMinus(i) {
+  elMinus[i].classList.toggle("hidden");
+  elPlus[i].classList.toggle("hidden");
+  elPar[i].classList.toggle("hidden");
+}
+for (let i = 0; i < elMinus.length; i++) {
+  let elpl = elMinus[i];
+  elpl.addEventListener("click", () => {
+    toggleToParaAndPlusAndMinus(i);
+  });
+}
