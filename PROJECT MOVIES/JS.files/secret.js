8588c16b13c1a765eeae37acf2049834ecace266
porat6766 <porat850@gmail.com>
2024-10-25 14:19:45 +0300

    project movies

---
+++ PROJECT MOVIES/JS.files/secret.js
@@ -0,0 +1,5 @@
+//keys for API request and save favorite movie
+const key_Movie = "9b097cebaa8eaf684959db8072f31e9f";
+const key_storage = "data-movie-17326565vyfd";
+
+export const secret = { key_Movie, key_storage };
