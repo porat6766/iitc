dd4be7a33d75c9718984c029427606f6172a725c
porat6766 <porat850@gmail.com>
2024-11-07 12:44:56 +0200

    ם

--- November 24/test/Vanilla JS DOM/script.js
+++ November 24/test/Vanilla JS DOM/script.js
@@ -1,13 +1,13 @@
 function showAlert() {
-    alert("Button clicked!");
+  alert("Button clicked!");
 }
 
 const button = document.getElementById("trigger-button");
 button.addEventListener("click", showAlert);
 
 function updateMessage() {
-    const messageBox = document.getElementById("message-box");
-    messageBox.textContent = "Hello, welcome to the exam project!";
+  const messageBox = document.querySelector(".message-box");
+  messageBox.textContent = "Hello, welcome to the exam project!";
 }
 
 updateMessage();
@@ -18,8 +18,8 @@ const ulElement = document.createElement("ul");
 ulElement.classList.add("user-list");
 
 for (let i = 0; i < userList.length; i++) {
-    const liElement = document.createElement("li");
-    ulElement.appendChild(liElement);
+  const liElement = document.createElement("li");
+  ulElement.appendChild(liElement);
 }
 
 document.body.appendChild(ulElement);
@@ -27,24 +27,52 @@ document.body.appendChild(ulElement);
 const items = document.querySelectorAll("li");
 
 for (let i = 0; i < items.length; i++) {
-    items[i].textContent = `Item ${i + 1}`;
+  items[i].textContent = `Item ${i + 1}`;
 }
 
-document.getElementById("simple-form").addEventListener("submit", function(event) {
-    event.preventDefault(); 
+document
+  .getElementById("simple-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
     const nameInput = document.getElementById("name").value;
-    
-    if (nameInput === "") {
-        alert("Name is required.");
-    } else {
-        alert(`Hello, ${nameInput}!`);
-    }
-});
 
+    if (nameInput === "") {
+      alert("Name is required.");
+    } else {
+      alert(`Hello, ${nameInput}!`);
+    }
+  });
 
 const settings = {
-    theme: "dark",
-    language: "en"
+  theme: "dark",
+  language: "en",
 };
 
-console.log(settings.color);  
+// console.log(settings.color);
+
+//37 שאלה א
+
+//39-45
+
+//31-34
+
+//18-27-29
+
+const greet = "hello word";
+
+function capitalizeFirstLetter(val) {
+  console.log(val.charAt(0).toUpperCase() + val.slice(1));
+}
+capitalizeFirstLetter(greet);
+lis = [1, 2, 3, 4, 5, 6];
+
+const evenNumber = lis.map((num) => {
+  if (num % 2 === 0) {
+    return num;
+  }
+});
+console.log(evenNumber);
+
+
+const array = ["jkhgjfhgd","kjhgjh","khjhg"]
+const longestWord = array.reduce(a,b)
