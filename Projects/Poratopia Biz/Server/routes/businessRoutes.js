5765c5309d2e4d88267234409753fbdd502d49f5
porat6766 <porat850@gmail.com>
2024-12-29 00:27:34 +0200

    jk

--- Projects/Poratopia Biz/Server/routes/businessRoutes.js
+++ Projects/Poratopia Biz/Server/routes/businessRoutes.js
@@ -41,7 +41,7 @@ router.put(
 router.delete(
   "/delete/:businessId",
   authenticateUser,
-  authorizeUser(["Gold", "Platinum"]),
+  authorizeUser(["Standard", "Gold", "Platinum"]),
   deleteBusiness
 );
 
