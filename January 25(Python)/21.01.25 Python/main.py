15161f3e195446a70f6825d946065dfb60434cb9
porat6766 <porat850@gmail.com>
2025-01-21 12:17:11 +0200

    pp

--- January 25(Python)/21.01.25 Python/main.py
+++ January 25(Python)/21.01.25 Python/main.py
@@ -1,1 +1,72 @@
-print("HELLO WORLD")
+'''
+FIRST TIME USING PYTHON
+'''
+
+# print("HELLO WORLD")
+
+# python is Awesome
+
+# if True:
+#    print("Python is Awesome")
+#    print("Maybe?")
+#    print("Maybe YES?")
+#    print("Maybe no?")
+#    print("Maybe i dont know?")
+
+
+'''
+we do project that gives
+to users to build the first web,
+ with comfort tools to do special design and in interactive way
+'''
+
+# name = "PORAT JOSEF ELIYA"
+# age = 23
+# Pi = 3.14
+
+# type number(js) = integer in python
+
+# message = name
+# print(message * 5)
+
+# text="My text"
+
+# try:
+#     print(text + 5)
+# except TypeError as e:
+#     print(f"Error: {e}")
+
+# string = "10"
+# number = 100
+# string = int(string)
+# print(type(string))
+# print(string)
+# print(string + number)
+
+# num1 = 1
+# num2 = 2
+# num3 = 3
+
+# print(num1 + num2 + num3)
+
+# if num1 > 0:
+#     print(num1+num2+num3)
+
+# userName = input("please tell me what your name:")
+
+# print(userName)
+
+# if "dudu" == "dkudu":
+#     print("jnj")
+
+# Loops
+# for i in range(5):
+#     print(i)
+
+
+for i in range(3, 5):
+    print(i)
+
+for i in range(3, 5):
+    for j in range(i, 8):
+        print(j, end="*")
