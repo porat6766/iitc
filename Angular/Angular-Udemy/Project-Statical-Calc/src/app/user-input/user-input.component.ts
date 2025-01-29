7a3689102d017d08003559c32175f44480bc53ff
porat6766 <porat850@gmail.com>
2025-01-29 09:49:13 +0200

    H

---
+++ Angular/Angular-Udemy/Project-Statical-Calc/src/app/user-input/user-input.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  enteredInitialInvestment = "0";
+  enteredAnnualInvestment = "0"
+  enteredExpectedReturn = "0"
+  enteredDuration = "0"
+
+
+  onSubmit() {
+    console.log("Submited");
+
+  }
+}
