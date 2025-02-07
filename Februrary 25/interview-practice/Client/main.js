34e423d513358bd31f7300fef0dd11d9bf0384e8
porat6766 <porat850@gmail.com>
2025-02-07 11:38:23 +0200

    RR

---
+++ Februrary 25/interview-practice/Client/main.js
@@ -0,0 +1,19 @@
+// function Person(name){
+//     this.name = name;
+//     this.say_Hi = function(RRR){
+//         console.log(RRR);
+
+//     }
+    
+// }
+
+// const P_1 =  Person("JJJJJ")
+
+// console.log(P_1.name)
+// P_1.say_Hi("bla") 
+
+// const numbers= [1,2,3]
+// const a= {}
+
+// a[numbers]= "3"
+// console.log(a);
---
+++ Februrary 25/interview-practice/Client/src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="bg-red-500">
+      oooo
+    </div>
+  )
+}
+
+export default App
